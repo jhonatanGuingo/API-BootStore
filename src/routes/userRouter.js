4cc3ebc913d17e772bc3e066d4c3257b2f51766d
ffukamizu <f.fukamizu@live.com>
2023-07-17 17:08:16 -0300

    naming hot fixes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,7 @@ import { validateSchema } from "../middlewares/validateSchema.js";
 import { schemaSigIn, schemaSignUp } from "../schemas/validate.schema.js";
 
 
-const router = Router();
-router.post("/signUp",validateSchema(schemaSignUp), signUp)
-router.post("/signIn", validateSchema(schemaSigIn), signIn)
-export default router;
+const authRouter = Router();
+authRouter.post("/signUp",validateSchema(schemaSignUp), signUp)
+authRouter.post("/signIn", validateSchema(schemaSigIn), signIn)
+export default authRouter;
