03a4b06761aa9d04ff754f892f408eb0dfb412f6
ffukamizu <f.fukamizu@live.com>
2023-07-17 17:02:27 -0300

    implemented /checkout endpoint fixes

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./userRouter.js"
 import productsRouter from "./productsRouter.js";
+import checkoutRouter from "./checkoutRouter.js";
 
 const router = Router();
 router.use(authRouter);
 router.use(productsRouter);
+router.use(checkoutRouter);
 
 export default router;
